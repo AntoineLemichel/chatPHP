4acd9b9ed20319dc67fcb8e629f04dd141db263e
AntoineLemichek <antoine.lemichel@newgates.fr>
2018-09-13 14:22:18 +0200

    Adding date and time for chat

--- chat.php
+++ chat.php
@@ -2,11 +2,11 @@
 include("db.php");
 if(isset($_POST['pseudo']) AND !empty($_POST['pseudo']) AND isset($_POST['message']) AND !empty($_POST['message'])){
   setcookie('pseudo', $_POST['pseudo'], time() + 365*24*3600, null, null, false, true);
-  $req = $bdd->prepare('INSERT INTO chat(pseudo, message) VALUES(:pseudo, :message)');
+  $req = $bdd->prepare('INSERT INTO chat(pseudo, message, timedate) VALUES(:pseudo, :message, NOW())');
 
   $req->execute(array(
     'pseudo' => htmlspecialchars($_POST['pseudo']),
-    'message' => $_POST['message']
+    'message' => $_POST['message'],
   ));
   header('Location: index.php');
 
